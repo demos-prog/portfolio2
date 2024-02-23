fe00f94172e5de868f6e72ef08047a73f04b5857
Дмитрий <demosne@gmail.com>
2024-02-23 09:46:54 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { useCallback, useEffect, useState } from 'react';
+import AOS from 'aos';
+import css from './App.module.css';
+
+
+function App() {
+  const [y, setY] = useState(document.scrollingElement ? document.scrollingElement.scrollHeight : 0);
+  const [scrollDirection, setScrollDirection] = useState(false);
+
+  const handleScroll = useCallback(() => {
+    if (y > window.scrollY) {
+      setScrollDirection(false);
+    } else if (y < window.scrollY) {
+      setScrollDirection(true);
+    }
+    setY(window.scrollY)
+  }, [y]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
+  AOS.init({
+    duration: 1200,
+  })
+
+
+  return (
+    <div className={css.container}>
+      <header className={scrollDirection ? css.nav_up : ''}>
+        <h1>Dmitry Burlyko</h1>
+      </header>
+      <div className={css.space}></div>
+
+      <div className={css.item}
+        data-aos="fade-right"
+        data-aos-anchor-placement="top-bottom"
+      >
+      </div>
+
+      <div className={css.space}></div>
+
+    </div>
+  );
+}
+
+export default App;
