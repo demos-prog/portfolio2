653cf4e257a7b5e5bfc3416cd4d492a0e9b74fe6
Дмитрий <demosne@gmail.com>
2024-02-26 16:02:16 +0300

    symbols

--- src/components/FlyingIcons/FlyingIcons.tsx
+++ src/components/FlyingIcons/FlyingIcons.tsx
@@ -8,6 +8,9 @@ const FlyingIcons: React.FC = () => {
   return (
     <div id={css.wrap}>
       <span id={css.dog}>@</span>
+      <span id={css.tag}>{`</>`}</span>
+      <span id={css.https}>https</span>
+      <span id={css.web}>WEB</span>
     </div>
   );
 };
