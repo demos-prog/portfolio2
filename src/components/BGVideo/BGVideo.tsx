1263b0e3e32cf7c6d2cead2d26646ddb65d731f1
Дмитрий <demosne@gmail.com>
2024-02-29 10:40:14 +0300

    parallax

--- src/components/BGVideo/BGVideo.tsx
+++ src/components/BGVideo/BGVideo.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import video from '../../assets/videos/fog.mp4';
 import css from './BGVideo.module.css';
 
 
 const BGVideo: React.FC = () => {
+  const [scrollY, setScrollY] = useState(0);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
-    <div id={css.video_background}>
+    <div style={{transform: `translateY(-${scrollY * 0.06}px)`}} id={css.video_background}>
       <video autoPlay loop muted>
         <source src={video} type="video/mp4" />
         Your browser does not support the video tag.
--- src/components/FlyingIcons/FlyingIcons.tsx
+++ src/components/FlyingIcons/FlyingIcons.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import css from './FlyingIcons.module.css';
 
 
 const FlyingIcons: React.FC = () => {
 
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div id={css.wrap}>
+    <div style={{ transform: `translateY(-${scrollY * 0.08}px)` }} id={css.wrap}>
       <span id={css.dog}>@</span>
       <span id={css.tag}>{`</>`}</span>
       <span id={css.https}>https</span>
