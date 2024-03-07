e6a0ab2a74f40d3f845262a443785e47b33fde42
Дмитрий <demosne@gmail.com>
2024-03-07 08:31:32 +0300

    height

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -32,16 +32,16 @@ const About: React.FC<AboutProps> = ({ lang }) => {
         <img id={css.brain} src={brainIcon} alt="" />
         <div id={css.wrapper}>
           <div id={css.skillsWrap}>
-            <Skill title={'HTML'} grade={4} />
-            <Skill title={'CSS'} grade={4} />
-            <Skill title={'SCSS'} grade={3} />
-            <Skill title={'CSS modulse'} grade={2} />
-            <Skill title={'JavaScript'} grade={4} />
+            <Skill title={'HTML'} grade={5} />
+            <Skill title={'CSS'} grade={5} />
+            <Skill title={'SCSS'} grade={4} />
+            <Skill title={'CSS modulse'} grade={3} />
+            <Skill title={'JavaScript'} grade={5} />
             <Skill title={'React JS'} grade={4} />
             <Skill title={'React Router'} grade={4} />
             <Skill title={'Next JS'} grade={2} />
-            <Skill title={'Styled Components'} grade={3} />
-            <Skill title={'TypeScript'} grade={3} />
+            <Skill title={'Styled Components'} grade={4} />
+            <Skill title={'TypeScript'} grade={4} />
             <Skill title={'Redux'} grade={2} />
           </div>
           <div id={css.skillsWrap}>
@@ -49,13 +49,13 @@ const About: React.FC<AboutProps> = ({ lang }) => {
             <Skill title={'Zustand'} grade={2} />
             <Skill title={'GitHub'} grade={3} />
             <Skill title={'Jest'} grade={2} />
-            <Skill title={'Vite'} grade={3} />
-            <Skill title={'Material UI'} grade={3} />
-            <Skill title={'Ant design'} grade={2} />
-            <Skill title={'REST API'} grade={3} />
-            <Skill title={'Graph QL'} grade={1} />
-            <Skill title={'Node JS'} grade={1} />
-            <Skill title={'Java'} grade={2} />
+            <Skill title={'Vite'} grade={4} />
+            <Skill title={'Material UI'} grade={4} />
+            <Skill title={'Ant design'} grade={3} />
+            <Skill title={'REST API'} grade={4} />
+            <Skill title={'Graph QL'} grade={2} />
+            <Skill title={'Node JS'} grade={2} />
+            <Skill title={'Java'} grade={3} />
             <Skill title={'English'} grade={3} />
           </div>
         </div>
